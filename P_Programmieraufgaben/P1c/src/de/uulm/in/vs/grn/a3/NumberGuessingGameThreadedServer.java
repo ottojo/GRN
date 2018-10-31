68cef78194c25c7a801c42f421275458bf305e63
ottojo <jottosmail@gmail.com>
2018-10-31 11:13:10 +0100

    server
    autoformat
    MAX_THREADS constant

--- P_Programmieraufgaben/P1c/src/de/uulm/in/vs/grn/a3/NumberGuessingGameThreadedServer.java
+++ P_Programmieraufgaben/P1c/src/de/uulm/in/vs/grn/a3/NumberGuessingGameThreadedServer.java
@@ -6,32 +6,25 @@ import java.net.Socket;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+public class NumberGuessingGameThreadedServer {
+    private static final int PORT = 5555;
+    private static final int MAX_THREADS = 4;
+    private static ServerSocket server;
+    private static Executor executor = Executors.newFixedThreadPool(MAX_THREADS);
 
-    public class NumberGuessingGameThreadedServer {
+    public static void main(String[] args) {
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Server started.");
 
-        private static final int PORT = 5555;
-        private static final int MAX_THREADS  = 4;
-        private static  ServerSocket server = null;
-        private static Executor executor = Executors.newFixedThreadPool(MAX_THREADS);
-
-        public static void main(String[]args) throws IOException {
-            try{
-                server = new ServerSocket(PORT);
-                System.out.println("Server started.");
-            }catch (IOException e){
-                e.printStackTrace();
+            while (!server.isClosed()) {
+                Socket soc = server.accept();
+                System.out.println("Client connected to Server");
+                Runnable workerThread = new NumberGuessingGameRequestHandler(soc);
+                executor.execute(workerThread);
             }
-
-            for(int i = 0; i < 4; i++){
-                while(!server.isClosed()) {
-                    Socket soc = server.accept();
-                    System.out.println("Client connected to Server");
-                    Runnable workerThread = new NumberGuessingGameRequestHandler(soc);
-                    executor.execute(workerThread);
-                }
-            }
-            
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
-
+}
